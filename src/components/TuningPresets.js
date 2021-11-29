592dbc0e6713d3bf05ea64451877078d8205ae00
pawanbenjamin <pawanbenjamin@gmail.com>
2021-11-29 16:45:08 -0500

    scale template sucsess

--- src/components/TuningPresets.js
+++ src/components/TuningPresets.js
@@ -18,33 +18,38 @@ const TuningPresets = ({
   const { state, dispatch } = useContext(store);
 
   const [currentScale, setCurrentScale] = useState([]);
+  const [currentFunc, setCurrentFunc] = useState(null);
 
-  let currentFun;
-
-  const setJhinjhoti = (e) => {
-    console.log("in the setter");
-    setSa("1/1");
-    setKomalRe("1/2");
-    setRe("1/3");
-    setKomalGa("1/4");
-    setGa("1/5");
-    setMa("1/6");
-    setTivraMa("1/7");
-    setPa("1/8");
-    setKomalDha("1/9");
-    setDha("1/10");
-    setKomalNi("1/11");
-    setNi("1/12");
+  const scales = {
+    Jhinjoti: function () {
+      console.log("in the setter");
+      setSa("1/1");
+      setKomalRe("1/2");
+      setRe("1/3");
+      setKomalGa("1/4");
+      setGa("1/5");
+      setMa("1/6");
+      setTivraMa("1/7");
+      setPa("1/8");
+      setKomalDha("1/9");
+      setDha("1/10");
+      setKomalNi("1/11");
+      setNi("1/12");
+    },
+    "Something Else": "A String",
   };
 
   return (
     <select
       onChange={(e) => {
-        console.log(e.target.value);
+        const theFunc = scales[e.target.value];
+        theFunc();
       }}
     >
-      <option value="Hello">Jhinjoti</option>
-      <option selected>Somthing Else</option>
+      <option value="Jhinjoti">Jhinjoti</option>
+      <option value="Something Else" selected>
+        Somthing Else
+      </option>
     </select>
   );
 };
